af56bb789d8e2f7ac7dd9bcb296a0896a957c786
navitat <anlon636@gmail.com>
2025-08-11 16:30:12 +0200

    feat: add and implement isAnon component

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,31 @@ import { Routes, Route } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 
+import IsPrivate from "./components/IsPrivate";
+import IsAnon from "./components/IsAnon";
+
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/signup"
+          element={
+            <IsAnon>
+              <SignupPage />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <IsAnon>
+              <LoginPage />
+            </IsAnon>
+          }
+        />
       </Routes>
       <Footer />
     </>
